f90a7e64d7996b4910b96eeaf55c31015a731c82
djoob20 <djoob20@gmail.com>
2022-04-28 20:17:54 +0200

    variable in JS

---
+++ variable.js
@@ -0,0 +1,9 @@
+var v1 = v2 = v3 = "3 variables";
+
+var number = 3;
+console.log("number est de type: " + typeof number);
+var text = "du text";
+console.log("text est de type: " + typeof text);
+var isTrue = true;
+console.log("isTrue est de type: " + typeof isTrue);
+console.log("rien est de type: " + typeof rien);
