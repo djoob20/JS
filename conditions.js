ca8469b961e85991f5078091c18a89e9b6108bf8
djoob20 <djoob20@gmail.com>
2022-04-29 20:00:17 +0200

    added second condition

--- conditions.js
+++ conditions.js
@@ -1,6 +1,13 @@
-var startMesssage = "Vôtre catégorie:", 
+/*var startMesssage = "Vôtre catégorie:", 
     endMessage,
     adult = confirm("Êtes vous adult?") ;
 
 endmessage = adult ? "18+": "-18";
-alert(startMesssage + endmessage);
+//alert(startMesssage + endmessage);
+*/
+
+var conditionTest = "fonctionnera ? fonctionnera pas";
+console.log(typeof conditionTest);
+if(conditionTest){
+    console.log("fonctionne"); 
+}
