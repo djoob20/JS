e78997d6d748eebb6fb72a11d80115c098967b1e
djoob20 <djoob20@gmail.com>
2022-04-30 23:08:34 +0200

    ajout de fonctions anonymes et isolées

--- fonctions/fonction.js
+++ fonctions/fonction.js
@@ -20,3 +20,34 @@ function fncWithReturn(){
 }
 
 console.log(fncWithReturn());
+
+//Fontions anonymes
+(function(){
+    console.log("Hallo fonction anonyme sans l'enrégistrer dans une variable");
+})();
+
+sayHallo = function(){
+    console.log("Hallo fonction anonyme en l'enrégistrant dans une variable");
+};
+
+sayHallo();
+
+var test = 'noir'; // On crée une variable « test » contenant le mot « noir »
+
+(function() { // Début de la zone isolée
+
+    var test = 'blanc'; // On crée une variable du même nom avec le contenu « blanc » dans la zone isolée
+  
+    console.log('Dans la zone isolée, la couleur est : ' + test);
+
+})(); // Fin de la zone isolée. Les variables créées dans cette zone sont détruites.
+
+console.log('Dans la zone non-isolée, la couleur est : ' + test); // Le texte final contient bien le mot « noir » vu que la « zone isolée » n'a aucune influence sur le reste du code
+
+var f = (fncX)=>{
+    console.log(fncX);
+}
+f("fncX");
+
+
+
