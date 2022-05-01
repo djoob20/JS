278c4bd5a4aac42d9a8a409807c4a1ba9c1a9c1a
Demba Diop <89271330+djoob20@users.noreply.github.com>
2022-05-01 15:23:17 +0200

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,28 @@
+let guests = ["Sarah Kate", "Audrey Simon", "Will Alexander"];
+console.log(guests[0]);
+
+let artistProfile = {
+    name: "Tao Perkngton",
+    ag: 27,
+    available: true
+};
+
+//enregistrer l'objet dans un tableau
+let allProfiles = [artistProfile];
+artistProfile.available = false;
+console.log(allProfiles);
+
+let guestLength = guests.length;
+console.log(guestLength);
+
+//ajouter un élément à la fin du tableau
+guests.push("Tao Perkington");
+console.log("After push: " + guests)
+
+//ajouter un élément au debut du Tableau
+guests.unshift("Tao Perkington");
+console.log("After unshift: " + guests);
+
+//supprimer le dernier élément du tableau
+guests.pop();
+console.log("After pop: "+ guests);
