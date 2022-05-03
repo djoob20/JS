54e09ed0336d2c931d0120866f04940001a9679a
djoob20 <djoob20@gmail.com>
2022-05-03 12:23:42 +0200

    added switch case and further condition check

--- conditions.js
+++ conditions.js
@@ -11,3 +11,46 @@ console.log(typeof conditionTest);
 if(conditionTest){
     console.log("fonctionne"); 
 }
+
+let v1 = 5;
+let v2  = "5";
+console.log("v1==v2:" + (v1==v2));  //true
+console.log("v1===v2:" + (v1===v2)); //false
+console.log("5=='5': " + (5=="5")); // true
+
+let firstUser = {
+    name: "Will Alexander",
+    age: 33,
+    accountLevel: "normal"
+};
+
+let secondUser = {
+    name: "Sarah Kate",
+    age: 21,
+    accountLevel: "premium"
+};
+
+let thirdUser = {
+    name: "Audrey Simon",
+    age: 27,
+    accountLevel: "mega-premium"
+};
+
+switch (firstUser.accountLevel) {
+    case 'normal':
+          console.log('You have a normal account!');
+    
+    break;
+    case 'premium':
+          console.log('You have a premium account!');
+    
+    break;
+    case 'mega-premium':
+          console.log('You have a mega premium account!');
+    break;
+    
+    default:
+          console.log('Unknown account type!');
+    
+
+}
