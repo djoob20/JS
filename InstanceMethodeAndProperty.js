8379e1c3781b30a9b43af53c66f045b48e8b52ef
djoob20 <djoob20@gmail.com>
2022-05-07 23:30:05 +0200

    added InstanceMethodeAndProperty.js and implementations

---
+++ InstanceMethodeAndProperty.js
@@ -0,0 +1,30 @@
+class BankAccount{
+    constructor(owner, balance){
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    showBalance(){
+        console.log("Solde: " + this.balance + " EUR");
+    }
+
+    deposit(amount){
+        console.log("Dépôt de " + amount + " EUR");
+        this.balance += amount;
+        this.showBalance();
+    }
+
+    withdrow(amount){
+        console.log("Retrait de "+ amount + " EUR");
+        if(this.balance >= amount){
+            this.balance -= amount;
+            this.showBalance();
+        }else{
+            console.log("Retrait refusé!!");
+        }
+    }
+}
+
+const newAccount = new BankAccount("Will Alexander", 500);
+newAccount.showBalance(); // imprime "Solde: 500 EUR" à la console
+newAccount.deposit(500);
