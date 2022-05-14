1a7b6985efd08ac6e66f32317a31200445c2b167
djoob20 <djoob20@gmail.com>
2022-05-14 22:47:57 +0200

    Modifiez des classes

--- ModifyDom.js
+++ ModifyDom.js
@@ -1,6 +1,15 @@
 "use strict";
 let main = document.getElementById('main');
 main.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
-alert("textContent: \n" + main.textContent);
-alert("innerText: \n" + main.innerText);
-alert("innerHTML: \n" + main.innerHTML);
+// alert("textContent: \n" + main.textContent);
+// alert("innerText: \n" + main.innerText);
+// alert("innerHTML: \n" + main.innerHTML);
+
+// Modifiez des classes
+//ajouter une classe à un élément
+const center = document.getElementsByClassName('center');
+center[0].classList.add('large');
+//center[0].classList.remove('center');
+center[0].classList.replace('rouge', 'bleu');
+alert(center[0].classList.contains('rouge'));
+center[0].classList.remove('center');
